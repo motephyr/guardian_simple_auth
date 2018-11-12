8fcd547d07a0243cd7991ce80e239cf803dd5984
motephyr <motephyr@gmail.com>
2018-11-12 16:53:44 +0800

    remove downcase

--- lib/session.ex
+++ lib/session.ex
@@ -1,7 +1,7 @@
 defmodule GuardianSimpleAuth.Session do
 
   def authenticate(model, %{"email" => email, "password" => password}) do
-    user = Application.get_env(:guardian_simple_auth, :repo).get_by(model, email: String.downcase(email))
+    user = Application.get_env(:guardian_simple_auth, :repo).get_by(model, email: email)
 
     case check_password(user, password) do
       true -> {:ok, user}
